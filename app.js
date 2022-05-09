6082760d6e5525584854bdc1a69c3eaa1efb551a
José Hernández <jose.ha.95@gmail.com>
2022-05-09 14:43:06 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('./config/yargs');
+const colors = require('colors');
+
+console.clear();
+
+// const [ , , arg3 = 'base=5'] = process.argv;
+// const [ , base = 5 ] = arg3.split('=');
+// console.log(process.argv);
+
+console.log(argv);
+
+// const base = 3;
+
+crearArchivo(argv.b, argv.l, argv.h)
+    .then( nombreArchivo => console.log(nombreArchivo.rainbow, 'creado') )
+    .catch( err => console.log(err) );
